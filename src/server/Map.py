cba7290ee7d1c2a5e76ac41b2154acd646ee4c70
Hermlon <hermlon@t-online.de>
2017-06-24 14:11:20 +0200

    Worked on Server, Map and SHot

--- src/server/Map.py
+++ src/server/Map.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env/ python3
+class Map(object):
+	
+	def __init__(self, area):
+		self.area = area
+		
+		
+	def updatePosition(self, player, position):
+		self.players[player] = position
+		
+	def getPostion(self, player):
+		return self.players[player]
+		
+	def updateShot(self, shot):
+		
--- src/server/Shot.py
+++ src/server/Shot.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env/ python3
+class Shot(object):
+	
+	def __init__(self, player):
+		self.player = player
+		
+	def setPosition(self, player, position):
+		self.players[player] = position
+		
+	def getPostion(self, player):
+		return self.players[player]
+		
+	def getPlayer(self):
+		return self.player
